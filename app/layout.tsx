623fe14682a02998cf55c90e083d12f60a35b666
Vybecheck <deorodesign@gmail.com>
2026-03-18 15:31:29 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import "./globals.css";
 import { AppProvider } from "./context";
-import Navbar from "./Navbar"; // Importuj náš nový Navbar
+
+export const metadata = {
+  title: "Vybecheck",
+  description: "Predict the culture.",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
       <body className="bg-zinc-950 text-white antialiased">
         <AppProvider>
-          <Navbar /> {/* Přidej ho sem NAD children */}
+          {/* Zde nesmí být žádný jiný Modal ani Header, vše obstará page.tsx */}
           {children}
         </AppProvider>
       </body>
